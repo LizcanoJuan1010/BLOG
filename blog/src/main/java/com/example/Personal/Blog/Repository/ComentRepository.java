d921bd00d88101da0a3178bbff743ded287e8168
Juan José <154391484+LizcanoJuan1010@users.noreply.github.com>
2024-10-17 22:50:25 -0500

    Eliminadas carpetas innecesarias

--- blog/src/main/java/com/example/Personal/Blog/Repository/ComentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.repository;
-
-import com.example.app.Coment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComentRepository extends JpaRepository<Coment, Long> {
-}
